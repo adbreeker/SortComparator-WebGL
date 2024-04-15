15fbc07c889064685c1c1b8261a0f27b01ae7c10
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2024-04-15 19:03:40 +0200

    small changes in optimized cocktail shaker sort

--- Assets/Scripts/Sorting/CocktailShakerSortOptimized.cs
+++ Assets/Scripts/Sorting/CocktailShakerSortOptimized.cs
@@ -7,53 +7,44 @@ public class CocktailShakerSortOptimized : SortingAlgorithm
 {
     protected override IEnumerator SortingCoroutine()
     {
-        bool swapped = false;
         //bounds between which the algorithm operates, (the bound is new start point, not last index before start point)
         int boundTop = _valueCount - 1;
         int boundBot = 0;
-        do
+        while(boundBot < boundTop)
         {
             yield return new WaitForFixedUpdate();
 
-            int newBound = boundTop;
+            int newBound = boundBot;
             for(int i = boundBot; i<boundTop; i++)
             {
                 yield return new WaitForFixedUpdate();
                 _toSort[i].ChangeColor(Color.red, 0);
                 if (_toSort[i] > _toSort[i+1])
                 {
-                    newBound = i;
+                    newBound = i + 1;
                     SwapTwoElementsByIndex(i, i + 1);
-                    swapped = true;
                 }
             }
-            ColorArea(boundTop, newBound + 1, Color.green);
-            boundTop = newBound;
+            ColorArea(boundTop, newBound, Color.green);
+            boundTop = newBound - 1;
 
-            if(!swapped)
-            {
-                break;
-            }
-
-            swapped = false;
-            newBound = boundBot;
             for(int i = boundTop; i>boundBot; i--)
             {
                 yield return new WaitForFixedUpdate();
                 _toSort[i].ChangeColor(Color.red, 0);
                 if (_toSort[i] < _toSort[i-1])
                 {
-                    newBound = i;
+                    newBound = i - 1;
                     SwapTwoElementsByIndex(i, i-1);
-                    swapped = true;
                 }
             }
-            ColorArea(boundBot, newBound - 1, Color.green);
-            boundBot = newBound;
+            ColorArea(boundBot, newBound, Color.green);
+            boundBot = newBound + 1;
             
-        } while (swapped);
+        }
         ColorArea(boundBot, boundTop, Color.green);
 
         _sortManager.SortFinished(_sortIndex);
+        _sortManager.StartSorting();
     }
 }
