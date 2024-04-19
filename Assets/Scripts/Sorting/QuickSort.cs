55762c616eb96784e61ddaaac690cca3d667caa7
Adam Cherek <111668308+adbreeker@users.noreply.github.com>
2024-04-19 13:23:16 +0200

    adding quick sort

---
+++ Assets/Scripts/Sorting/QuickSort.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSort : SortingAlgorithm
+{
+    int partitionIndex;
+
+    protected override IEnumerator SortingCoroutine()
+    {
+        yield return StartCoroutine(QuickSortCoroutine(0, _toSort.Length - 1));
+        //ColorArea(0, _valueCount - 1, Color.green);
+        _sortManager.SortFinished(_sortIndex);
+    }
+
+    private IEnumerator QuickSortCoroutine(int low, int high)
+    {
+        yield return new WaitForFixedUpdate();
+        if (low < high)
+        {
+            yield return StartCoroutine(Partition(low, high));
+
+            int partitionIndexTemp = partitionIndex;
+
+            yield return StartCoroutine(QuickSortCoroutine(low, partitionIndexTemp - 1));
+            yield return StartCoroutine(QuickSortCoroutine(partitionIndexTemp + 1, high));
+        }
+        else
+        {
+            if(high == _valueCount - 1) { _toSort[high].ChangeColor(Color.green); }
+            else { ColorArea(high + 1, low, Color.green); }
+        }
+    }
+
+    IEnumerator Partition(int low, int high)
+    {
+        ValueVisualiser pivot = _toSort[high];
+        pivot.ChangeColor(Color.red, 0);
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            yield return new WaitForFixedUpdate();
+            pivot.ChangeColor(Color.red, 0);
+            if (_toSort[j] < pivot)
+            {
+                i++;
+                ColorArea(i, j, Color.yellow, 0);
+                _toSort[j].ChangeColor(Color.red, 0);
+                _toSort[i].ChangeColor(Color.red, 0);
+                SwapTwoElementsByIndex(i, j);
+            }
+        }
+        yield return new WaitForFixedUpdate();
+        _toSort[i + 1].ChangeColor(Color.red, 0);
+        _toSort[high].ChangeColor(Color.red, 0);
+        SwapTwoElementsByIndex(i + 1, high);
+        partitionIndex = i + 1;
+    }
+}
